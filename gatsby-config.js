43f98c2fec057b8be6d7ee634f2c57be6ac2e243
Maize <maize@freakmail.de>
2022-04-16 17:06:43 +0200

    123

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `DMFK 2022`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
+      name: `Someone`,
+      summary: `else.`,
     },
   },
   plugins: [
